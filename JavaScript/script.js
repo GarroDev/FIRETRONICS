c43d7ec6cef1aea3508a57408f8199c523eeaf03
MateoLondonoE <mateo_londono@elpoli.edu.co>
2024-04-01 17:58:54 -0500

    Revert "Merge branch 'main' of https://github.com/GarroDev/FIRETRONICS"
    This reverts commit 1725646a2eac224116ca9616075fc5795aee862c, reversing
    changes made to 4fc1a0c36b1796eed05ff90ee71f25ace47a8fb6.

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+// Cambia de pestaña cada 5 segundos
+var tabs = ['tab1', 'tab2', 'tab3', 'tab4']; // Agregar más identificadores de pestañas si es necesario
+var currentIndex = 0;
+
+setInterval(function() {
+    $('#' + tabs[currentIndex]).removeClass('active');
+    $('#' + tabs[currentIndex]).tab('show');
+    currentIndex = (currentIndex + 1) % tabs.length;
+}, 5000); // Cambia cada 5 segundos 
