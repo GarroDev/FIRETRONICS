26706c111553b850ce1f021acc769ab5c3d9a48f
MateoLondonoE <mateo_londono@elpoli.edu.co>
2024-06-02 21:30:30 -0500

    Working in log out and dark theme

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -43,6 +43,12 @@ window.onload = cambiarValorAlmacenamiento;
 // Execute funtion on page reload
 window.addEventListener('load', mostrarOcultarElementos);
 
+function LogOutF() {
+    sessionStorage.setItem('loggedIn', 'false');
+    sessionStorage.setItem('profile', '');
+}
+
+
 // Switches tabs every 5 seconds
 var tabs = ['tab1', 'tab2', 'tab3', 'tab4']; // Add more tab identifiers if needed
 var currentIndex = 0;
@@ -197,3 +203,21 @@ if (Profile === '2') {
 } else {
     // alert('Profile value is: no data');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    // Recuperar el tema guardado en sessionStorage
+    const currentTheme = sessionStorage.getItem('theme') || 'light';
+    if (currentTheme === 'dark') {
+        body.classList.add('dark-theme');
+    }
+
+    // Añadir el evento de clic al botón
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+        const newTheme = body.classList.contains('dark-theme') ? 'dark' : 'light';
+        sessionStorage.setItem('theme', newTheme);
+    });
+});
